182c08345e56f4e2e64b6320523845dc24da8740
Rotavlas <119404660+Rotavlas@users.noreply.github.com>
2023-01-16 15:15:32 +0100

    Add files via upload

--- TD_-_API_Fetch_-_etudiant/assets/script.js
+++ TD_-_API_Fetch_-_etudiant/assets/script.js
@@ -1,43 +1,44 @@
-async function getStarsFromVote(note) {
-    let starEmpty = '<ion-icon name="star-outline"></ion-icon>'
-    let halfStar = '<ion-icon name="star-half-outline"></ion-icon>'
-    let fullStar = '<ion-icon name="star"></ion-icon>'
-    let code , countdown 
-    countdown = 0 
-        if (note == 0) {
-            code = 5 * starEmpty
-        } else if (note == 0.5) {
-            code = halfStar + ( 4 * starEmpty )
-            return code
-        } else if ( note == 1 ) {
-            code = fullStar + ( 4 * starEmpty )
-            return code
-        } else if ( note == 1.5 ) {
-            code = fullStar + halfStar + ( 3 * starEmpty )
-            return code
-        } else if ( note == 2 ) {
-            code = ( 2 * fullStar ) + ( 3 * starEmpty )
-            return code
-        } else if ( note == 2.5 ) {
-            code = ( 2 * fullStar ) + halfStar + ( 2 * starEmpty )
-            return code
-        } else if ( note == 3 ) {
-            code = ( 3 * fullStar ) + ( 2 * starEmpty )
-            return code
-        } else if ( note = 3.5 ) {
-            code = ( 3 * fullStar ) + halfStar + starEmpty
-            return code
-        } else if ( note = 4 ) {
-            code = ( 4 * fullStar ) + starEmpty
-            return code
-        } else if ( note = 4.5 ) {
-            code = ( 4 * fullStar ) + halfStar
-            return code
-        } else if ( note = 5 ) {
-            code = 5 * fullStar
-            return code
-        }
-    }
+// async function getStarsFromVote(note) {
+//     let starEmpty = '<ion-icon name="star-outline"></ion-icon>'
+//     let halfStar = '<ion-icon name="star-half-outline"></ion-icon>'
+//     let fullStar = '<ion-icon name="star"></ion-icon>'
+//     let code , countdown 
+//     countdown = 0 
+//         if (note == 0) {
+//             code = 5 * starEmpty
+//         } else if (note == 0.5) {
+//             code = halfStar + ( 4 * starEmpty )
+//             return code
+//         } else if ( note == 1 ) {
+//             code = fullStar + ( 4 * starEmpty )
+//             return code
+//         } else if ( note == 1.5 ) {
+//             code = fullStar + halfStar + ( 3 * starEmpty )
+//             return code
+//         } else if ( note == 2 ) {
+//             code = ( 2 * fullStar ) + ( 3 * starEmpty )
+//             return code
+//         } else if ( note == 2.5 ) {
+//             code = ( 2 * fullStar ) + halfStar + ( 2 * starEmpty )
+//             return code
+//         } else if ( note == 3 ) {
+//             code = ( 3 * fullStar ) + ( 2 * starEmpty )
+//             return code
+//         } else if ( note = 3.5 ) {
+//             code = ( 3 * fullStar ) + halfStar + starEmpty
+//             return code
+//         } else if ( note = 4 ) {
+//             code = ( 4 * fullStar ) + starEmpty
+//             return code
+//         } else if ( note = 4.5 ) {
+//             code = ( 4 * fullStar ) + halfStar
+//             return code
+//         } else if ( note = 5 ) {
+//             code = 5 * fullStar
+//             return code
+//         }
+//     }
+
 
 
 
@@ -54,7 +55,7 @@ function getArticle(movie) {
                     <div class="card-body">
                       <h5 class="card-title">${movie.original_title}</h5>
                       <p class="card-text">${description}</p>
-                      <p class="card-text"><small class="text-muted"><ion-icon name="star-outline"></ion-icon><ion-icon name="star-outline"></ion-icon><ion-icon name="star-outline"></ion-icon><ion-icon name="star-outline"></ion-icon><ion-icon name="star-outline"></ion-icon></small></p>
+                      <p class="card-text">${getStarsFromVote(movie.vote_average)}</p>
                     </div>
                   </div>
                 </div>
@@ -62,7 +63,29 @@ function getArticle(movie) {
     return article
 }
 
-fetch('assets/data/movies.json')
-    
+function getStarsFromVote(score) {
+    score = parseInt(score+0.5)/2;
+    container = document.createElement("div");
+    let n_fill = parseInt(score);
+    let n_half = parseInt(score-n_fill+0.5);
+    for (let i = 0; i < 5; i++) {
+        let star = document.createElement("i");
+        if (i < n_fill) {star.classList.add("fa-solid", "fa-star");}
+        else if (i < n_fill+n_half) {star.classList.add("fa-solid", "fa-star-half-stroke");}
+        else {star.classList.add("fa-regular", "fa-star");}
+        container.append(star);
+    }
+    return container.innerHTML;
+}
+
+fetch(new Request ('assets/data/movies.json'))
+    .then((result) => result.json())
+    .then((result) => {
+        let movie_article = document.getElementById("movie-article")
+        movies = result.results.map(getArticle)
+        for (i of movies ) {movie_article.append(i)}
+    })
+
+
 
 
